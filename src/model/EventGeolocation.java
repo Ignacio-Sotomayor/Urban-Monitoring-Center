69bed7bf61265dceaed1371e4ceee93273812797
Ignacio <sotomayornachi05@gmail.com>
2025-07-15 14:44:41 -0300

    fix: missing import of package

---
+++ src/model/EventGeolocation.java
@@ -0,0 +1,42 @@
+package model;
+
+public class EventGeolocation {
+ private String id;
+ private String dateHour;
+ private String address;
+
+ public EventGeolocation(String id, String dateHour, String address) {
+  this.id = id;
+  this.dateHour = dateHour;
+  this.address = address;
+ }
+
+ public String getId() {
+  return id;
+ }
+
+ public void setId(String id) {
+  this.id = id;
+ }
+
+ public String getDateHour() {
+  return dateHour;
+ }
+
+ public void setDateHour(String dateHour) {
+  this.dateHour = dateHour;
+ }
+
+ public String getAddress() {
+  return address;
+ }
+
+ public void setAddress(String address) {
+  this.address = address;
+ }
+
+ @Override
+ public String toString() {
+  return "EventGeolocation [id=" + id + ", dateHour=" + dateHour + ", address=" + address + "]";
+ }
+}
---
+++ src/model/Fine.java
@@ -0,0 +1,64 @@
+package model;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Fine {
+    private double amount;
+    private int scoring;
+    private Set<Photo> photos; // no duplicates
+    private EventGeolocation eventGeolocation;
+    private InfractionType infractionType;
+
+    public Fine(double amount, int scoring, EventGeolocation eventGeolocation, InfractionType infractionType) {
+        this.amount = amount;
+        this.scoring = scoring;
+        this.photos = new HashSet<>();
+        this.eventGeolocation = eventGeolocation;
+        this.infractionType = infractionType;
+    }
+
+    public void addPhoto(Photo photo) {
+        photos.add(photo);
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getScoring() {
+        return scoring;
+    }
+
+    public void setScoring(int scoring) {
+        this.scoring = scoring;
+    }
+
+    public Set<Photo> getPhotos() {
+        return photos;
+    }
+
+    public EventGeolocation getEventGeolocation() {
+        return eventGeolocation;
+    }
+
+    public void setEventGeolocation(EventGeolocation eventGeolocation) {
+        this.eventGeolocation = eventGeolocation;
+    }
+
+    public InfractionType getInfractionType() {
+        return infractionType;
+    }
+
+    public void setInfractionType(InfractionType infractionType) {
+        this.infractionType = infractionType;
+    }
+
+    @Override
+    public String toString() {
+        return "Fine [amount=" + amount + ", scoring=" + scoring + "]";
+    }
+}
